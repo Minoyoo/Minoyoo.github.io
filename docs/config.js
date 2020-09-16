34d1944a618013397256374afec66c07fa269eb1
Minoyoo <756345195@qq.com>
2020-09-16 22:47:56 +0800

    theme

--- docs/config.js
+++ docs/config.js
@@ -1,9 +1,12 @@
 docute.init({
+    //主题
+    detectSystemDarkTheme: true,
+    darkThemeToggler: true,
     // 标题栏
     nav: [
         // 首页
         {title: '首页', path: '/'},
         // 中文文档
-        {title: '博客', path: 'https://wugenqiang.github.io/'}
+        //{title: '博客', path: 'https://wugenqiang.github.io/'}
       ]
   })
